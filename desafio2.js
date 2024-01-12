1f460f3c9b759014fb85ebf76a4c233539fe2014
jessica98rios <146677620+jessica98rios@users.noreply.github.com>
2024-01-12 09:22:31 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,38 +1,43 @@
-let saldo = diferenca(50, 24)
-let nivel = " "
-
-switch (true){
-    
-    case saldo>100:
-    nivel = "Imortal"
-    break
-    
-    case saldo>90:
-    nivel = "Lendário"
-    break
-
-	case saldo>80:
-    nivel = "Diamante"
-    break
-    
-    case saldo>50:
-    nivel = "Ouro"
-    break
-    
-    case saldo>20:
-    nivel = "Prata"
-    break
-    
-    case saldo>10:
-    nivel = "Bronze"
-    break
-    
-	default:
-    nivel = "Ferro" 
-}
+let saldo = subtrair(500, 24)
+let nivel = nivelar(saldo)
 
 console.log(`A heroína tem saldo de ${saldo} vitórias e está no nível ${nivel}.`)
 
-function diferenca(vitorias, derrotas){
+function subtrair(vitorias, derrotas){
 	return vitorias - derrotas
+}
+
+function nivelar(saldo){
+    let nivel = " "
+    
+    switch (true){
+    
+    	case saldo>100:
+    	nivel = "Imortal"
+    	break
+    
+    	case saldo>90:
+    	nivel = "Lendário"
+    	break
+
+    	case saldo>80:
+    	nivel = "Diamante"
+    	break
+    
+    	case saldo>50:
+    	nivel = "Ouro"
+    	break
+    
+    	case saldo>20:
+    	nivel = "Prata"
+    	break
+    
+    	case saldo>10:
+    	nivel = "Bronze"
+    	break
+    
+    	default:
+    	nivel = "Ferro" 
+    }
+    return nivel
 }
